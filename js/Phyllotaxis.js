638744e9d18b7e36717907ce97884ee9fe34faf9
Sahil Khanna <mail@sahilkhanna.org>
2017-11-29 00:26:03 +1100

    Some UI fix and frame rate indicator

--- js/Phyllotaxis.js
+++ js/Phyllotaxis.js
@@ -2,18 +2,18 @@ var n = 0;
 var angle=137.5;
 var shapesize = 1;
 var c = shapesize*2;
+var framerate;
 //console.log(c,shapesize);
 function setup() {
-  canvas = createCanvas(windowWidth, windowHeight);
+  canvas = createCanvas(windowWidth*0.8, windowHeight*0.8);
   angleMode(DEGREES);
   colorMode(RGB);
   background(0);
-
   angle = map(random(),0,1,0,360);
+  framerate=createP('');
 }
 
 function draw() {
-
   var a = n *angle;
   var r = c * sqrt(n);
   var x = r * cos(a) + width /2;
@@ -21,6 +21,6 @@ function draw() {
   fill(map(noise(x),0,1,0,255),map(noise(y),0,1,0,255),map(noise(sqrt((x*x)+(y*y))),0,1,0,255));
   noStroke();
   ellipse(x,y,shapesize,shapesize);
-
   n++;
+  framerate.html(floor(frameRate()))
 }
